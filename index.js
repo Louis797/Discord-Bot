816fcce799964a2aad8e6aa0b26b837e42ffe449
Louis Leonardo de Sa <loui.leosa@gmail.com>
2021-07-16 15:48:46 +0200

    setbsfdtbg

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log(`logged in as ${client.user.tag}`);
+	message.send(`I just arrived ! How's everyone`);
 });
 
 client.on('message', async (message) => {
 	if (message.content == 'ping') {
 		message.reply('pong !');
+	} else if (message.content == 'bite') {
+		message.reply(`eh oh dites donc c'est pas bien de dire ça là oh !`);
 	}
 });
 client.login(process.env.TOKEN);
