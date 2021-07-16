c7934f446e7f3d98c19b212b78d89350aa058f00
Louis Leonardo de Sa <loui.leosa@gmail.com>
2021-07-16 19:10:13 +0200

    stbsdfb

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+motsInterdits = ['bite', 'connard', 'pute', 'chier', 'merde'];
+
 client.on('ready', () => {
 	console.log(`logged in as ${client.user.tag}`);
 	message.send(`I just arrived ! How's everyone`);
@@ -10,8 +12,14 @@ client.on('ready', () => {
 client.on('message', async (message) => {
 	if (message.content == 'ping') {
 		message.reply('pong !');
-	} else if (message.content == 'bite') {
-		message.reply(`eh oh dites donc c'est pas bien de dire ça là oh !`);
+	}
+	for (var i = 0; i < forbidenWords.length; i++) {
+		if (message.content.includes(motsInterdits[i])) {
+			message.reply(
+				`eh oh dites donc c'est pas bien de dire ça non mais on est où là.`
+			);
+			break;
+		}
 	}
 });
 client.login(process.env.TOKEN);
