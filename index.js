a7293c3226c186db7f134ccb04bf5070f956739b
Louis Leonardo de Sa <loui.leosa@gmail.com>
2021-07-18 12:37:26 +0200

    ersv

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ client.on('message', async (message) => {
 		message.react('6️⃣');
 		message.react('9️⃣');
 		message.reply('nice');
+	} else if (message.mentions.has(client.user.id)) {
+		message.reply('Hello there!');
 	}
 	for (var i = 0; i < motsInterdits.length; i++) {
 		if (message.content.toLowerCase().includes(motsInterdits[i])) {
