9adf4a051ad091f279fcda98047f7698d8aa8771
Louis Leonardo de Sa <loui.leosa@gmail.com>
2021-07-20 15:28:42 +0200

    'zgtefd

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ client.on('message', async (message) => {
 		message.content.length < 7
 	) {
 		message.reply('pong !');
-		message.channel.send(message.content);
 		message.channel.send(`Hello Mister ${message.author}`);
 		client.channels.cache.get(`866376359962214450`).send(`Text`);
 	} else if (
