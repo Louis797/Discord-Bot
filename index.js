67a3a7a8fa860ea73924bb599e8be8ac4a2ae227
Louis Leonardo de Sa <loui.leosa@gmail.com>
2021-11-02 20:37:19 +0100

    htrebfvs

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ client.on('ready', () => {
 	.send('Je suis connecté désormais ahahahha');
 });
 
+client.users.fetch('386810143268143106').then((user) => {
+	user.send('Yooooooo !')})
+
 client.on('message', async (message) => {
-	client.users.fetch('386810143268143106').then((user) => {
-		user.send('Yooooooo !')})
 	if (
 		message.content.toLowerCase().includes('ping') &&
 		message.content.length < 7
