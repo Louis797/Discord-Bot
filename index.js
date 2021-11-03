11f3e9321ec6eddbfca4bcb955089a9f38d1bce2
Louis Leonardo de Sa <loui.leosa@gmail.com>
2021-11-03 19:56:58 +0100

    (yhrtbd

--- index.js
+++ index.js
@@ -1,86 +1,87 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const client = new Discord.Client({
-	partials: ['MESSAGE'],
+  partials: ["MESSAGE"],
 });
 
-client.on('ready', () => {
-	client.channels.cache
-	.get(`866376359962214450`)
-	.send('Je suis connecté désormais ahahahha');
-});
-
-let x = 4
-let y = 0
-let id = '905517661415436328'
-
+let motsInterdits = ["connard"];
+let x = 4;
+let y = 0;
+let id = "905517661415436328";
 setInterval(Thing, 60000);
 
 function Thing() {
-	if (x >= 2) {
-		x -= 1
-		client.users.fetch(id).then((user) => {
-			user.send(x)})
-	} else {
-		if (y == 0) {
-			y += 1
-			x -= 1
-			client.users.fetch(id).then((user) => {
-				user.send(x)
-				user.send('Youpi !')
-			})
-		}
-	}
+  if (x >= 2) {
+    x -= 1;
+    client.users.fetch(id).then((user) => {
+      user.send(x);
+    });
+  } else {
+    if (y == 0) {
+      y += 1;
+      x -= 1;
+      client.users.fetch(id).then((user) => {
+        user.send(x);
+        user.send("Youpi !");
+      });
+    }
+  }
 }
 
-client.on('message', async (message) => {
-	if (
-		message.content.toLowerCase().includes('ping') &&
-		message.content.length < 7
-	) {
-		message.reply('pong !');
-	} else if (
-		message.content.toLowerCase().includes('pong') &&
-		message.content.length < 7
-	) {
-		message.reply('...ping..?');
-	} else if (message.content == 'salut') {
-		message.channel.send('Bonjour !');
-	} else if (message.content == 'comment ça va ?') {
-		message.channel.send('ça va :grin: ');
-	} else if (message.content == 'vive les bots') {
-		message.react('❤️');
-	} else if (message.content == '69') {
-		message.react('6️⃣');
-		message.react('9️⃣');
-		message.reply('Nice.');
-	}
-	for (var i = 0; i < motsInterdits.length; i++) {
-		if (message.content.toLowerCase().includes(motsInterdits[i])) {
-			message.author.send(
-				`eh oh dites donc c'est pas bien de dire ça non mais on est où là.`
-			);
-			break;
-		}
-	}
+client.on("ready", () => {
+  client.channels.cache
+    .get(`866376359962214450`)
+    .send("Je suis connecté désormais ahahahha");
 });
 
-client.on('messageDelete', (message) => {
-	message.author.send(`Le message : "${message.content}",  de ${message.author} a été supprimé.`);
+client.on("message", async (message) => {
+  if (
+    message.content.toLowerCase().includes("ping") &&
+    message.content.length < 7
+  ) {
+    message.reply("pong !");
+  } else if (
+    message.content.toLowerCase().includes("pong") &&
+    message.content.length < 7
+  ) {
+    message.reply("...ping..?");
+  } else if (message.content == "salut") {
+    message.channel.send("Bonjour !");
+  } else if (message.content == "comment ça va ?") {
+    message.channel.send("ça va :grin: ");
+  } else if (message.content == "vive les bots") {
+    message.react("❤️");
+  } else if (message.content == "69") {
+    message.react("6️⃣");
+    message.react("9️⃣");
+    message.reply("Nice.");
+  }
+  for (var i = 0; i < motsInterdits.length; i++) {
+    if (message.content.toLowerCase().includes(motsInterdits[i])) {
+      message.author.send(
+        `eh oh dites donc c'est pas bien de dire ça non mais on est où là.`
+      );
+      break;
+    }
+  }
 });
 
-client.on('channelCreate', function (channel) {
-	client.channels.cache
-		.get(`866376359962214450`)
-		.send(`channelCreate: ${channel}`);
+client.on("messageDelete", (message) => {
+  message.author.send(
+    `Le message : "${message.content}",  de ${message.author} a été supprimé.`
+  );
 });
 
-client.on('channelDelete', function (channel) {
-	client.channels.cache
-		.get(`866376359962214450`)
-		.send(`channelDelete: ${channel}`);
+client.on("channelCreate", function (channel) {
+  client.channels.cache
+    .get(`866376359962214450`)
+    .send(`channelCreate: ${channel}`);
+});
+
+client.on("channelDelete", function (channel) {
+  client.channels.cache
+    .get(`866376359962214450`)
+    .send(`channelDelete: ${channel}`);
 });
 
 client.login(process.env.TOKEN);
-
-motsInterdits = ['connard'];
